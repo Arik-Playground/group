21d3374a83174e2a7462bd1598ab95cd00e291fb
Arik Nassi <WubbaLubba@users.noreply.github.com>
2021-08-08 21:39:07 +0300

    Creation benchmarks

---
+++ benchmarking/creation/main.cpp
@@ -0,0 +1,117 @@
+#include <tuple>
+#include <utility>
+
+#include "../../include/nassgroup/group.hpp"
+
+#include <benchmark/benchmark.h>
+
+#ifndef BENCH_NO_INLINE
+
+#ifdef __clang__
+#define BENCH_NO_INLINE __attribute__ ((noinline))
+#elif __GNUC__ 
+#define BENCH_NO_INLINE __attribute__ ((noinline))
+#endif //__GNUC__
+
+#endif // BENCH_NO_INLINE
+
+struct value_tt
+{
+    static volatile size_t created;
+    static volatile size_t copied;
+    static volatile size_t moved;
+
+    size_t value;
+
+    value_tt():
+    value{ created + (copied << 16) + (moved << 32) }
+    { created = created + 1; }
+
+    value_tt(const value_tt&):
+    value{ created + (copied << 16) + (moved << 32) }
+    { copied = copied + 1; }
+
+    value_tt(value_tt&&):
+    value{ created + (copied << 16) + (moved << 32) }
+    { moved = moved + 1; }
+};
+
+namespace group_utils
+{
+    template <size_t... Idxs>
+    BENCH_NO_INLINE
+    auto make(std::index_sequence<Idxs...>)
+    {
+        return nass::make_group<decltype(Idxs)...>(Idxs...);
+    }
+
+    template <size_t... Idxs>
+    BENCH_NO_INLINE
+    auto make_value(std::index_sequence<Idxs...>)
+    {
+        return nass::make_value_group(Idxs...);
+    }
+
+    template <size_t... Idxs>
+    BENCH_NO_INLINE
+    auto make_forward(std::index_sequence<Idxs...>)
+    {
+        return [](auto... vals)
+        {        
+            return nass::make_forward_group(vals...);
+        }(Idxs...);
+    }
+
+    template <size_t... Idxs>
+    BENCH_NO_INLINE
+    auto make_from(std::index_sequence<Idxs...>)
+    {
+        return nass::make_group_from((Idxs, []{ return 5; })...);
+    }
+}
+
+namespace tuple_utils
+{
+    template <size_t... Idxs>
+    BENCH_NO_INLINE
+    auto make(std::index_sequence<Idxs...>)
+    {
+        return std::tuple{Idxs...};
+    }
+
+    template <size_t... Idxs>
+    BENCH_NO_INLINE
+    auto make_value(std::index_sequence<Idxs...>)
+    {
+        return std::make_tuple(Idxs...);
+    }
+
+    template <size_t... Idxs>
+    BENCH_NO_INLINE
+    auto make_forward(std::index_sequence<Idxs...>)
+    {
+        return [](auto... vals)
+        {        
+            return std::forward_as_tuple(vals...);
+        }(Idxs...);
+    }
+}
+
+auto BM_make_group = [](auto& bstate) { for (auto b: bstate) group_utils::make(std::make_index_sequence<300>()); };
+auto BM_make_value_group = [](auto& bstate) { for (auto b: bstate) group_utils::make_value(std::make_index_sequence<300>()); };
+auto BM_make_forward_group = [](auto& bstate) { for (auto b: bstate) group_utils::make_forward(std::make_index_sequence<300>()); };
+auto BM_make_group_from = [](auto& bstate) { for (auto b: bstate) group_utils::make_from(std::make_index_sequence<300>()); };
+BENCHMARK(BM_make_group);
+BENCHMARK(BM_make_value_group);
+BENCHMARK(BM_make_forward_group);
+BENCHMARK(BM_make_group_from);
+
+auto BM_make_tuple = [](auto& bstate) { for (auto b: bstate) tuple_utils::make(std::make_index_sequence<300>()); };
+auto BM_make_value_tuple = [](auto& bstate) { for (auto b: bstate) tuple_utils::make_value(std::make_index_sequence<300>()); };
+auto BM_make_forward_tuple = [](auto& bstate) { for (auto b: bstate) tuple_utils::make_forward(std::make_index_sequence<300>()); };
+BENCHMARK(BM_make_tuple);
+BENCHMARK(BM_make_value_tuple);
+BENCHMARK(BM_make_forward_tuple);
+
+
+BENCHMARK_MAIN();
--- benchmarking/giant_group/tuple.cpp
+++ benchmarking/giant_group/tuple.cpp
@@ -12,4 +12,35 @@ auto create_giant_tuple(std::index_sequence<Idxs...>)
 #define NASSGROUP_BENCHMARK_GIANT_NUMBER 1000
 #endif // NASSGROUP_BENCHMARK_GIANT_NUMBER
 
-extern const auto testee = create_giant_tuple(std::make_index_sequence<NASSGROUP_BENCHMARK_GIANT_NUMBER>());
+// extern const auto testee = create_giant_tuple(std::make_index_sequence<NASSGROUP_BENCHMARK_GIANT_NUMBER>());
+
+struct value_t
+{
+    static size_t created;
+    static size_t copied;
+    static size_t moved;
+
+    size_t value;
+
+    value_t():
+    value{ created++ + (copied << 16) + (moved << 32) }
+    {}
+
+    value_t(const value_t&):
+    value{ created + ((copied++) << 16) + (moved << 32) }
+    {}
+
+    value_t(value_t&&):
+    value{ created + (copied << 16) + ((moved++) << 32) }
+    {}
+};
+
+size_t value_t::created = 0;
+size_t value_t::copied = 0;
+size_t value_t::moved = 0;
+
+static value_t dummy{};
+auto make()
+{
+    return std::tuple<value_t,value_t, value_t&, const value_t&, value_t&&>(dummy, std::move(dummy), dummy, dummy, std::move(dummy));
+}
