d4f07f232c332bcbb550e5c1ee4fb0ebcad57287
Arik Nassi <WubbaLubba@users.noreply.github.com>
2021-08-10 01:44:02 +0300

    reorder some files

--- compile_benchmarking/construction_and_get/run.sh
+++
@@ -1,5 +0,0 @@
-echo "group time"
-time g++-11 -c compile_benchmarking/construction_and_get/group.cpp --std=c++2a -O9 -o /dev/stdout > /dev/null 
-
-echo "tuple time"
-time g++-11 -c compile_benchmarking/construction_and_get/tuple.cpp --std=c++2a -O9 -o /dev/stdout > /dev/null 
